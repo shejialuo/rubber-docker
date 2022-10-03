9ca8acce7d2dd110f254d5b0ac9e283f85e8db96
shejialuo <shejialuo@gmail.com>
2022-10-03 15:45:43 +0800

    Finish level 01_chroot_image

--- rd.py
+++ rd.py
@@ -1,8 +1,57 @@
 from __future__ import print_function
 
-import click
 import os
+import tarfile
+import uuid
+
+import click
 import traceback
+
+import linux
+
+# Set the encode mode to be uft-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def _get_image_path(image_name, image_dir, image_suffix='tar'):
+    return os.path.join(image_dir, os.extsep.join([image_name, image_suffix]))
+
+
+def _get_container_path(container_id, container_dir, *subdir_names):
+    return os.path.join(container_dir, container_id, *subdir_names)
+
+
+def create_container_root(image_name, image_dir, container_id, container_dir):
+    """Create a container root by extracting an image into a new directory
+
+    Usage:
+    new_root = create_container_root(
+        image_name, image_dir, container_id, container_dir)
+
+    @param image_name: the image name to extract
+    @param image_dir: the directory to lookup image tarballs in
+    @param container_id: the unique container id
+    @param container_dir: the base directory of newly generated container
+                          directories
+    @retrun: new container root directory
+    @rtype: str
+    """
+    image_path = _get_image_path(image_name, image_dir)
+    container_root = _get_container_path(container_id, container_dir, 'rootfs')
+
+    assert os.path.exists(image_path), "unable to locate image %s" % image_name
+
+    if not os.path.exists(container_root):
+        os.makedirs(container_root)
+
+    with tarfile.open(image_path) as t:
+        # Fun fact: tar files may contain *nix devices! *facepalm*
+        members = [m for m in t.getmembers()
+                   if m.type not in (tarfile.CHRTYPE, tarfile.BLKTYPE)]
+        t.extractall(container_root, members=members)
+
+    return container_root
 
 
 @click.group()
@@ -10,26 +59,55 @@ def cli():
     pass
 
 
-def contain(command):
-    # NOTE: command is an array (the first element is path/file, and the entire
-    #       array is exec's args)
+def contain(command, image_name, image_dir, container_id, container_dir):
+
+    new_root_path = create_container_root(image_name, image_dir, container_id, container_dir)
+
+    # We should mount `proc`, `sys` and recursive mount `/dev`.
+    # This is like the way as the command line.
+    # We could use `cat /proc/filesystems` to see the virtual
+    # filesystem
+    linux.mount('proc', os.path.join(new_root_path, 'proc'), 'proc', 0, '')
+    linux.mount('sysfs', os.path.join(new_root_path, 'sys'), 'sysfs', 0, '')
+    linux.mount('tmpfs', os.path.join(new_root_path, 'dev'), 'tmpfs',
+                linux.MS_NOSUID | linux.MS_STRICTATIME, 'mode=755')
+
+    devpts_path = os.path.join(new_root_path, 'dev', 'pts')
+    if not os.path.exists(devpts_path):
+        os.makedirs(devpts_path)
+        linux.mount('devpts', devpts_path, 'devpts', 0, '')
+    for i, dev in enumerate(['stdin', 'stdout', 'stderr']):
+        os.symlink('/proc/self/fd/%d' % i, os.path.join(new_root_path, 'dev', dev))
+
+    os.chroot(new_root_path)
+    # After `os.chroot`, the working diretcory would be corrupted.
+    # So we should use `os.chdir`to change the working directory
+    os.chdir('/')
+
     os.execvp(command[0], command)
 
 
 @cli.command(context_settings=dict(ignore_unknown_options=True,))
+@click.option('--image-name', '-i', help='Image name', default='ubuntu')
+@click.option('--image-dir', help='Images directory',
+              default='/workshop/images')
+@click.option('--container-dir', help='Containers directory',
+              default='/workshop/containers')
 @click.argument('Command', required=True, nargs=-1)
-def run(command):
+def run(image_name, image_dir, container_dir, command):
+    container_id = str(uuid.uuid4())
     pid = os.fork()
     if pid == 0:
         # This is the child, we'll try to do some containment here
         try:
-            contain(command)
+            contain(command, image_name, image_dir, container_id,
+                    container_dir)
         except Exception:
             traceback.print_exc()
             os._exit(1)  # something went wrong in contain()
 
     # This is the parent, pid contains the PID of the forked process
-    # wait for the forked child and fetch the exit status
+    # wait for the forked child, fetch the exit status
     _, status = os.waitpid(pid, 0)
     print('{} exited with status {}'.format(pid, status))
 
